992ec5ee6386557daf7ce533e6b2cce807302cde
Markus Wichmann <nullplan@gmx.net>
2021-12-18 22:01:51 +0100

    Add atan.

---
+++ src/math/i386/atan.c
@@ -0,0 +1,15 @@
+#include "libm.h"
+
+double atan(double x)
+{
+    uint32_t ix;
+    GET_HIGH_WORD(ix, x);
+    ix &= 0x7fffffff;
+    if (ix < 0x00100000)
+    {
+        FORCE_EVAL(x * x);
+        return x;
+    }
+    __asm__("fpatan" : "=t"(x) : "0"(1.0L), "u"(x) : "st(1)");
+    return x;
+}
