4dd68212aec3bd90cdc386eebb79671d3a641896
Markus Wichmann <nullplan@gmx.net>
2021-12-18 19:13:13 +0100

    Convert acos.s.
    This was pretty straightforward. Using long double for the temporary
    objects to give gcc no cause to switch to SSE or any such ill-advised
    thing.

---
+++ src/math/i386/acos.c
@@ -0,0 +1,14 @@
+#include <math.h>
+
+/* use acos(x) = atan2(fabs(sqrt((1-x)*(1+x))), x) */
+double acos(double x)
+{
+    long double one;
+    __asm__("fld1" : "=t"(one));
+    long double radicand = (one - x) * (one + x);
+    long double numerator;
+    /* fabs to fix sign of zero (matters in downward rounding mode) */
+    __asm__("fsqrt; fabs" : "=t"(numerator) : "0"(radicand));
+    __asm__("fpatan" : "=t"(x) : "0"(x), "u"(numerator) : "st(1)");
+    return x;
+}
