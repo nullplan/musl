00a213e25e0125bf9e649790e98caf9abcd6eef2
Markus Wichmann <nullplan@gmx.net>
2021-12-18 20:56:43 +0100

    Fix indentation and tabs to musl standard.

--- src/math/i386/acos.c
+++ src/math/i386/acos.c
@@ -3,12 +3,13 @@
 /* use acos(x) = atan2(fabs(sqrt((1-x)*(1+x))), x) */
 double acos(double x)
 {
-    long double one;
-    __asm__("fld1" : "=t"(one));
-    long double radicand = (one - x) * (one + x);
-    long double numerator;
-    /* fabs to fix sign of zero (matters in downward rounding mode) */
-    __asm__("fsqrt; fabs" : "=t"(numerator) : "0"(radicand));
-    __asm__("fpatan" : "=t"(x) : "0"(x), "u"(numerator) : "st(1)");
-    return x;
+	long double lx = x;
+	long double one;
+	__asm__("fld1" : "=t"(one));
+	long double radicand = (one - lx) * (one + lx);
+	long double numerator;
+	/* fabs to fix sign of zero (matters in downward rounding mode) */
+	__asm__("fsqrt; fabs" : "=t"(numerator) : "0"(radicand));
+	__asm__("fpatan" : "=t"(lx) : "0"(lx), "u"(numerator) : "st(1)");
+	return lx;
 }
