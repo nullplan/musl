ee2f81e2c32c5e30eb843db6e3af7e8400f59b4f
Markus Wichmann <nullplan@gmx.net>
2021-12-19 15:29:36 +0100

    Convert log.

---
+++ src/math/i386/log.c
@@ -0,0 +1,10 @@
+#include "libm.h"
+
+double log(double x)
+{
+	long double ln2;
+	long double res;
+	__asm__("fldln2" : "=t"(ln2));
+	__asm__("fyl2x" : "=t"(res) : "0"(x), "u"(ln2) : "st(1)");
+	return res;
+}
