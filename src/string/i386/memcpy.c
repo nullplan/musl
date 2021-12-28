98bb0dea057df6ae060b33542d4fd7956dc78d79
Markus Wichmann <nullplan@gmx.net>
2021-12-28 09:51:30 +0100

    Replace rep with loops in memcpy. Replace mem clobber with mem output.
    The original code had the alignment loop and the remainder loop as
    actual loops, not as rep prefixes. Not sure why, but apparently there
    was a reason, so the loops are now reproduced.
    
    I also went back and removed the memory clobbers in favor of memory
    outputs. This tells the compiler exactly what memory is changed. In the
    case of isolated compilation, this should not matter much, since the
    memory copied is never read back, so the compiler can't make any smart
    decisions about loading it, but in case of LTO, this may be entirely
    different.

--- src/string/i386/memcpy.c
+++ src/string/i386/memcpy.c
@@ -5,16 +5,17 @@
 void *memcpy(void *restrict dst, const void *restrict src, size_t len)
 {
 	void *odst = dst;
-	/* little known fact about the rep prefix: if ECX==0, it turns into a NOP. */
-	if (len >= 4) {
-		size_t to_align = (-(uintptr_t)dst) & 3;
-		len -= to_align;
-		__asm__("rep; movsb" : "+c"(to_align), "+S"(src), "+D"(dst) :: "memory");
-	}
+	if (len >= 4)
+		while ((uintptr_t)dst & 3) {
+			len--;
+			__asm__("movsb" : "=m"(*(char*)dst), "+S"(src), "+D"(dst));
+		}
+
 	size_t words = len >> 2;
-	__asm__("rep; movsl" : "+c"(words), "+S"(src), "+D"(dst) :: "memory");
+	__asm__("rep; movsl" : "=m"(*(uint32_t(*)[words])dst), "+c"(words), "+S"(src), "+D"(dst));
 	len &= 3;
-	__asm__("rep; movsb" : "+c"(len), "+S"(src), "+D"(dst) :: "memory");
+	while (len--)
+		__asm__("movsb" : "=m"(*(char*)dst), "+S"(src), "+D"(dst));
 	return odst;
 }
 
