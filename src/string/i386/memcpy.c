0cbf09cb862712b70a81360c0f87f1d246f914fe
Markus Wichmann <nullplan@gmx.net>
2021-12-28 10:00:10 +0100

    Add input memory constraints to memcpy.
    Of course, movsb not only writes but also reads memory. As before, it
    really doesn't matter for isolated compilation (and indeed the compiled
    assembler code is exactly the same as before), but it may matter with
    LTO, and I at least don't want to stand in the way of that.

--- src/string/i386/memcpy.c
+++ src/string/i386/memcpy.c
@@ -8,14 +8,14 @@ void *memcpy(void *restrict dst, const void *restrict src, size_t len)
 	if (len >= 4)
 		while ((uintptr_t)dst & 3) {
 			len--;
-			__asm__("movsb" : "=m"(*(char*)dst), "+S"(src), "+D"(dst));
+			__asm__("movsb" : "=m"(*(char*)dst), "+S"(src), "+D"(dst) : "m"(*(char*)src));
 		}
 
 	size_t words = len >> 2;
-	__asm__("rep; movsl" : "=m"(*(uint32_t(*)[words])dst), "+c"(words), "+S"(src), "+D"(dst));
+	__asm__("rep; movsl" : "=m"(*(uint32_t(*)[words])dst), "+c"(words), "+S"(src), "+D"(dst) : "m"(*(uint32_t(*)[words])src));
 	len &= 3;
 	while (len--)
-		__asm__("movsb" : "=m"(*(char*)dst), "+S"(src), "+D"(dst));
+		__asm__("movsb" : "=m"(*(char*)dst), "+S"(src), "+D"(dst) : "m"(*(char*)src));
 	return odst;
 }
 
